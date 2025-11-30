b500c45c1746711920177d81e444b80f3d44fff5
Pavlo Malysh <mal.pawlo@gmail.com>
2025-12-01 01:26:35 +0200

    add getCatalogServer

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,11 +1,10 @@
 import css from "./CatalogPage.module.css"
-import { getCatalog } from "@/lib/api/clientApi";
-import { getBrandsServer } from "@/lib/api/serverApi";
+import { getBrandsServer, getCatalogServer } from "@/lib/api/serverApi";
 import CatalogPageClient from "./CatalogPage.client";
 
 const CatalogPage = async () => {
     const brands = await getBrandsServer();
-    const initialData = await getCatalog(1, 12, {});
+    const initialData = await getCatalogServer(1, 12, {});
 
     return (
         <div className={`container ${css.catalogPage}`}>
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,5 +1,6 @@
-import { Car } from "@/types/car";
+import { Car, SearchForm } from "@/types/car";
 import { nextServer } from "./api";
+import { CatalogListResponse } from "./clientApi";
 
 export const getBrandsServer = async () => {
     const res = await nextServer.get<string[]>("/brands");
@@ -9,4 +10,20 @@ export const getBrandsServer = async () => {
 export const getCarByIdServer = async (id: string) => {
     const res = await nextServer.get<Car>(`/cars/${id}`);
     return res.data
+}
+
+export const getCatalogServer = async (page: number, limit: number, searchQuery: SearchForm) => {
+    const filteredQuery = Object.fromEntries(
+        Object.entries(searchQuery).filter(([, value]) => value)
+    );
+
+    const res = await nextServer.get<CatalogListResponse>("/cars", {
+        params: {
+            page,
+            limit,
+            ...filteredQuery
+        }
+    });
+
+    return res.data;
 }

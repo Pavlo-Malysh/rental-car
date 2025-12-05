cbbed75a3a7b43b87348354e0ec6b2099c25d3fe
Pavlo Malysh <mal.pawlo@gmail.com>
2025-12-05 22:49:38 +0200

    add adaptive styles to the hero page

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
 
 
     return (
-        <div className={`${css.bgImage} container`}>
+        <div className={`${css.bgImage}`}>
             <h1 className={css.title}>Find your perfect rental car</h1>
             <p className={css.text}>Reliable and budget-friendly rentals for any journey</p>
             <Link href="/catalog" className={css.heroLink}>View Catalog</Link>
